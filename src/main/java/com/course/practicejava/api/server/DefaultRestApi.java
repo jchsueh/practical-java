0c9859ab4138d65b88cc96f23d68d7f2340d466d
jchsueh <jchsueh@gmail.com>
2021-07-04 18:33:44 +0800

    Add log to DefaultRestApi
    Change system.out.print to spring log

--- src/main/java/com/course/practicejava/api/server/DefaultRestApi.java
+++ src/main/java/com/course/practicejava/api/server/DefaultRestApi.java
@@ -1,6 +1,10 @@
 package com.course.practicejava.api.server;
 
+import java.time.LocalTime;
+
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,10 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value ="/api")
 public class DefaultRestApi {
+	
+	private Logger LOG = LoggerFactory.getLogger(DefaultRestApi.class);
 
     @GetMapping(value = "/welcome")
     public String welcome(){
-        System.out.println(StringUtils.join("Hello","this is","Spring Boot","Rest API"));
+        LOG.info(StringUtils.join("Hello","this is","Spring Boot","Rest API"));
         return "Welcome to Spring Boot";
+    }
+    
+    @GetMapping(value = "/time")
+    public String time() {
+    	return LocalTime.now().toString();
     }
 }
